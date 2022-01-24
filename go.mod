79389a77f93992ec9ab334d7243bc256495e000b
ervitis <vitomarti@gmail.com>
2022-01-24 23:30:41 +0100

    feat: invoices automation

--- go.mod
+++ go.mod
@@ -7,13 +7,13 @@ require (
 	github.com/go-openapi/runtime v0.21.0
 	github.com/go-openapi/strfmt v0.21.1
 	github.com/go-openapi/swag v0.19.15
-	github.com/go-openapi/validate v0.20.3
+	github.com/sakirsensoy/genv v1.0.1
 	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8
-	google.golang.org/api v0.30.0
+	google.golang.org/api v0.65.0
 )
 
 require (
-	cloud.google.com/go v0.65.0 // indirect
+	cloud.google.com/go/compute v1.1.0 // indirect
 	github.com/PuerkitoBio/purell v1.1.1 // indirect
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
 	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
@@ -22,24 +22,24 @@ require (
 	github.com/go-openapi/jsonreference v0.19.6 // indirect
 	github.com/go-openapi/loads v0.21.0 // indirect
 	github.com/go-openapi/spec v0.20.4 // indirect
+	github.com/go-openapi/validate v0.20.3 // indirect
 	github.com/go-stack/stack v1.8.1 // indirect
-	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
-	github.com/golang/protobuf v1.4.2 // indirect
-	github.com/googleapis/gax-go/v2 v2.0.5 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/googleapis/gax-go/v2 v2.1.1 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mitchellh/mapstructure v1.4.3 // indirect
 	github.com/oklog/ulid v1.3.1 // indirect
 	github.com/opentracing/opentracing-go v1.2.0 // indirect
-	github.com/sakirsensoy/genv v1.0.1 // indirect
 	go.mongodb.org/mongo-driver v1.8.2 // indirect
-	go.opencensus.io v0.22.4 // indirect
+	go.opencensus.io v0.23.0 // indirect
 	golang.org/x/net v0.0.0-20220121210141-e204ce36a2ba // indirect
-	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
 	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/appengine v1.6.6 // indirect
-	google.golang.org/genproto v0.0.0-20200825200019-8632dd797987 // indirect
-	google.golang.org/grpc v1.31.0 // indirect
-	google.golang.org/protobuf v1.25.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20220118154757-00ab72f36ad5 // indirect
+	google.golang.org/grpc v1.43.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- invoices/cmd/main.go
+++ invoices/cmd/main.go
@@ -1,5 +1,111 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"github.com/ervitis/freelancetools/credentials"
+	"google.golang.org/api/drive/v3"
+	"google.golang.org/api/option"
+	"google.golang.org/api/sheets/v4"
+	"log"
+	"time"
+)
 
+const (
+	folderId = "" // TODO set folder ID
+
+	invoiceDateLayout = "02-01-2006"
+
+	templateNameInvoice = "template"
+)
+
+var (
+	invoiceName = "" // TODO set invoice name
+)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	credentialManagerClient := credentials.New()
+	if err := credentialManagerClient.SetConfigWithScopes(sheets.SpreadsheetsScope, drive.DriveScope, drive.DriveMetadataScope, drive.DriveFileScope, drive.DriveAppdataScope); err != nil {
+		log.Fatalln(err)
+	}
+
+	sheetsService, err := sheets.NewService(ctx, option.WithHTTPClient(credentialManagerClient.GetClient()))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	driveService, err := drive.NewService(ctx, option.WithHTTPClient(credentialManagerClient.GetClient()))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	listFiles := driveService.Files.List().IncludeItemsFromAllDrives(true).SupportsAllDrives(true)
+
+	listBillingModel, err := listFiles.Q(fmt.Sprintf(`"%s" in parents and name contains "MODELO FACTURA" and trashed = false`, folderId)).Do()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if len(listBillingModel.Files) == 0 {
+		log.Println("files not found")
+		return
+	}
+
+	listInvoices, err := listFiles.Q(fmt.Sprintf(`"%s" in parents and not name contains "MODELO FACTURA" and trashed = false`, folderId)).Do()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	billingModel := listBillingModel.Files[0]
+	invoiceName = fmt.Sprintf(invoiceName, len(listInvoices.Files)+1, time.Now().Format(invoiceDateLayout))
+
+	copiedFile, err := driveService.Files.Copy(billingModel.Id, &drive.File{
+		MimeType: "application/vnd.google-apps.spreadsheet",
+		Name:     invoiceName,
+	}).Do()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	invoice, err := sheetsService.Spreadsheets.Get(copiedFile.Id).Do()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	valueRange := make([]*sheets.ValueRange, 0)
+	row := map[string]interface{}{
+		"H3":  fmt.Sprintf("%d", len(listInvoices.Files)+1),
+		"H4":  time.Now().Format(invoiceDateLayout),
+		"H5":  time.Now().Format(invoiceDateLayout), // TODO set last day of month
+		"H8":  "",                                   // TODO company name
+		"H9":  "",                                   // TODO company address
+		"B15": 15.0,                                 // TODO quantity of hours
+		"C15": "",                                   // TODO Description
+		"E15": 1.0,                                  // TODO set price in yen
+	}
+
+	for k, v := range row {
+		data := make([][]interface{}, 0)
+		data2 := make([]interface{}, 0)
+		data2 = append(data2, v)
+		data = append(data, data2)
+		vr := &sheets.ValueRange{
+			Range:  fmt.Sprintf("%s!%s", templateNameInvoice, k),
+			Values: data,
+		}
+		valueRange = append(valueRange, vr)
+	}
+
+	resp, err := sheetsService.Spreadsheets.Values.BatchUpdate(invoice.SpreadsheetId, &sheets.BatchUpdateValuesRequest{
+		Data:             valueRange,
+		ValueInputOption: "RAW",
+	}).Do()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(resp)
 }
